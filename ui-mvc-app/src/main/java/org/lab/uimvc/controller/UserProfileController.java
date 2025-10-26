019c9be5aa1ecaddafe8997a623760646c847cd8
Stanislav Kuprienko <stasi.architekk@gmail.com>
2025-10-27 02:26:12 +0600

    ui-mvc-app: update user page

--- ui-mvc-app/src/main/java/org/lab/uimvc/controller/UserProfileController.java
+++ ui-mvc-app/src/main/java/org/lab/uimvc/controller/UserProfileController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/main/profile")
+@RequestMapping("/main/user")
 public class UserProfileController extends MvcControllerUtil {
 
     private final UserService userService;
