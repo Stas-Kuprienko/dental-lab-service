4b81b59ac7f1267ddcf896313bf67824e0ad31b4
Stanislav Kuprienko <stasi.architekk@gmail.com>
2025-11-16 23:43:03 +0600

    add observability, need to fix

--- create-db.sql
+++ create-db.sql
@@ -1,2 +1,5 @@
 SELECT 'CREATE DATABASE dental_lab'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dental_lab');
+
+SELECT 'CREATE DATABASE dental_keycloak'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dental_keycloak');
